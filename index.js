6ff87be390ab23a85553c274d58360310bf78fbd
Jason McCandless <jhmccandless1105@gmail.com>
2021-06-22 23:05:31 -0500

    working on home page

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const http = require("http");
 const express = require("express");
 const bodyParser = require("body-parser");
 const { execPath } = require("process");
+const es6Renderer = require("express-es6-template-engine");
 
 const app = express();
 const server = http.createServer(app);
+app.engine("html", es6Renderer);
+app.set("views", "views");
+app.set("view engine", "html");
 
 const hostname = "127.0.0.1";
 const port = 3785;
@@ -14,9 +18,9 @@ const port = 3785;
 ///// app logic
 
 app.get("/", (req, res) => {
-  res.send(console.log("working"));
+  res.render("home");
 });
-app.use(express.static("public"));
+
 app.use(bodyParser.urlencoded());
 
 server.listen(port, hostname, () => {
