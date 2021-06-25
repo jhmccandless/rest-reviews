7a75f05e1188b970db0a8bec3f5720f1cf52b388
Jason McCandless <jhmccandless1105@gmail.com>
2021-06-24 20:34:23 -0500

    some more touches

--- index.js
+++ index.js
@@ -3,24 +3,30 @@
 const http = require("http");
 const express = require("express");
 const bodyParser = require("body-parser");
-const { execPath } = require("process");
+// const { execPath } = require("process");
 const es6Renderer = require("express-es6-template-engine");
+const pgp = require("pg-promise")({});
+const db = pgp({ database: "jasonmccandless" });
 
 const app = express();
 const server = http.createServer(app);
 app.engine("html", es6Renderer);
-app.set("views", "views");
+app.set("views", "templates");
 app.set("view engine", "html");
 
 const hostname = "127.0.0.1";
 const port = 3785;
 
-// app.use(express.static("public"));
-app.use(express.static(__dirname));
+app.use("/public", express.static("public"));
 app.use(bodyParser.urlencoded());
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/search/", async (req, res) => {
+  const info = await db.query("SELECT * FROM review");
+  res.render("search_results");
 });
 
 server.listen(port, hostname, () => {
